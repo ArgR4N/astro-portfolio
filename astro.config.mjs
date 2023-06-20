b3a9394a42dfc7b730cdbcec777fda2aa8371981
ArgR4N <ramiroreinaldo12@gmail.com>
2023-06-20 08:39:16 -0300

    🔩 Configure astro  for github pages deploy

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,4 +5,6 @@ import tailwind from "@astrojs/tailwind";
 // https://astro.build/config
 export default defineConfig({
   integrations: [react(), tailwind()],
+  site: 'https://argr4n.github.io',
+  base: '/astro-portfolio',
 });
