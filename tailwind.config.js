3147dbe6812d4592ce5e5b07e529a44b17537ee5
r4n <ramiroreinaldo12@gmail.com>
2023-06-20 08:25:05 -0300

    🔩 Add config files (astro, tailwind, ts, .gitignore, ...)

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["src/**/*.tsx", "src/**/*.ts", "src/**/*.jsx", "src/**/*.js", "src/**/*.astro"],
+  theme: {
+    extend: {
+    },
+  },
+  plugins: [],
+}
+
