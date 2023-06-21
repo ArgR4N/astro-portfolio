55c8d3b312bbfa78bd447ff0f812ccd5a560952d
r4n <ramiroreinaldo12@gmail.com>
2023-06-21 14:21:42 -0300

    📄 Edit config files

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,18 @@
 import { defineConfig } from 'astro/config';
 import react from "@astrojs/react";
 import tailwind from "@astrojs/tailwind";
+import vercel from "@astrojs/vercel/static";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [react(), tailwind()],
   site: 'https://argr4n.github.io',
   base: '/astro-portfolio',
+  vite: {
+    ssr: {
+      noExternal: ['tailwindcss']
+    }
+  },
+  output: "static",
+  adapter: vercel()
 });
