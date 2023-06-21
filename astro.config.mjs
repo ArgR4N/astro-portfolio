42469eafdcd5a1881ef11a4af9e2815f552b108d
r4n <ramiroreinaldo12@gmail.com>
2023-06-21 15:12:51 -0300

    📝 Fixed styles in vercel

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,18 +1,18 @@
 import { defineConfig } from 'astro/config';
 import react from "@astrojs/react";
 import tailwind from "@astrojs/tailwind";
+import mdx from '@astrojs/mdx';
+import sitemap from '@astrojs/sitemap';
 import vercel from "@astrojs/vercel/static";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [react(), tailwind()],
-  site: 'https://argr4n.github.io',
-  base: '/astro-portfolio',
   vite: {
     ssr: {
       noExternal: ['tailwindcss']
     }
   },
   output: "static",
+  integrations: [mdx(), sitemap(),react(), tailwind()],
   adapter: vercel()
 });
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,8 @@
 /** @type {import('tailwindcss').Config} */
-export default {
-  content: ["src/**/*.tsx", "src/**/*.ts", "src/**/*.jsx", "src/**/*.js", "src/**/*.astro"],
-  theme: {
-    extend: {
-    },
-  },
-  plugins: [],
+module.exports = {
+	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+	theme: {
+		extend: {},
+	},
+	plugins: [],
 }
-
